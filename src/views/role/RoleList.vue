e6b7a274bb84a5ca49630b3991677ef64e948dd0
tim@ubuntu@asus <1547681531@qq.com>
2021-01-24 23:25:51 +0800

    恢复 其他组件（IconSelector,业务布局：树目录表格，用户列表，角色列表，权限列表）

--- src/views/role/RoleList.vue
+++ src/views/role/RoleList.vue
@@ -19,42 +19,54 @@
           </div>
           <a-form :form="form" :layout="isMobile() ? 'vertical' : 'horizontal'">
             <a-form-item label="唯一键">
-              <a-input v-decorator="[ 'id', {rules: [{ required: true, message: 'Please input unique key!' }]} ]" placeholder="请填写唯一键" />
+              <a-input
+                v-decorator="['id', { rules: [{ required: true, message: 'Please input unique key!' }] }]"
+                placeholder="请填写唯一键"
+              />
             </a-form-item>
 
             <a-form-item label="角色名称">
-              <a-input v-decorator="[ 'name', {rules: [{ required: true, message: 'Please input role name!' }]} ]" placeholder="请填写角色名称" />
+              <a-input
+                v-decorator="['name', { rules: [{ required: true, message: 'Please input role name!' }] }]"
+                placeholder="请填写角色名称"
+              />
             </a-form-item>
 
             <a-form-item label="状态">
-              <a-select v-decorator="[ 'status', {rules: []} ]">
+              <a-select v-decorator="['status', { rules: [] }]">
                 <a-select-option :value="1">正常</a-select-option>
                 <a-select-option :value="2">禁用</a-select-option>
               </a-select>
             </a-form-item>
 
             <a-form-item label="备注说明">
-              <a-textarea :row="3" v-decorator="[ 'describe', {rules: [{ required: true, message: 'Please input role name!' }]} ]" placeholder="请填写角色名称" />
+              <a-textarea
+                :row="3"
+                v-decorator="['describe', { rules: [{ required: true, message: 'Please input role name!' }] }]"
+                placeholder="请填写角色名称"
+              />
             </a-form-item>
 
             <a-form-item label="拥有权限">
               <a-row :gutter="16" v-for="(permission, index) in permissions" :key="index">
-                <a-col :xl="4" :lg="24">
-                  {{ permission.name }}：
-                </a-col>
+                <a-col :xl="4" :lg="24"> {{ permission.name }}： </a-col>
                 <a-col :xl="20" :lg="24">
                   <a-checkbox
                     v-if="permission.actionsOptions.length > 0"
                     :indeterminate="permission.indeterminate"
                     :checked="permission.checkedAll"
-                    @change="onChangeCheckAll($event, permission)">
+                    @change="onChangeCheckAll($event, permission)"
+                  >
                     全选
                   </a-checkbox>
-                  <a-checkbox-group :options="permission.actionsOptions" v-model="permission.selected" @change="onChangeCheck(permission)" />
+                  <a-checkbox-group
+                    :options="permission.actionsOptions"
+                    v-model="permission.selected"
+                    @change="onChangeCheck(permission)"
+                  />
                 </a-col>
               </a-row>
             </a-form-item>
-
           </a-form>
         </div>
       </a-col>
@@ -65,7 +77,6 @@
 <script>
 import pick from 'lodash.pick'
 import { getRoleList, getPermissions } from '@/api/manage'
-import { actionToObject } from '@/utils/permissions'
 import { baseMixin } from '@/store/app-mixin'
 
 export default {
@@ -108,13 +119,13 @@ export default {
       if (this.mdl.permissions && this.permissions) {
         // 先处理要勾选的权限结构
         const permissionsAction = {}
-        this.mdl.permissions.forEach(permission => {
-          permissionsAction[permission.permissionId] = permission.actionEntitySet.map(entity => entity.action)
+        this.mdl.permissions.forEach((permission) => {
+          permissionsAction[permission.permissionId] = permission.actionEntitySet.map((entity) => entity.action)
         })
 
         console.log('permissionsAction', permissionsAction)
         // 把权限表遍历一遍，设定要勾选的权限 action
-        this.permissions.forEach(permission => {
+        this.permissions.forEach((permission) => {
           const selected = permissionsAction[permission.id]
           permission.selected = selected || []
           this.onChangeCheck(permission)
@@ -130,27 +141,28 @@ export default {
     },
 
     onChangeCheck (permission) {
-      permission.indeterminate = !!permission.selected.length && (permission.selected.length < permission.actionsOptions.length)
+      permission.indeterminate =
+        !!permission.selected.length && permission.selected.length < permission.actionsOptions.length
       permission.checkedAll = permission.selected.length === permission.actionsOptions.length
     },
     onChangeCheckAll (e, permission) {
       console.log('permission:', permission)
 
       Object.assign(permission, {
-        selected: e.target.checked ? permission.actionsOptions.map(obj => obj.value) : [],
+        selected: e.target.checked ? permission.actionsOptions.map((obj) => obj.value) : [],
         indeterminate: false,
         checkedAll: e.target.checked
       })
     },
     loadPermissions () {
-      getPermissions().then(res => {
+      getPermissions().then((res) => {
         const result = res.result
-        this.permissions = result.map(permission => {
-          const options = actionToObject(permission.actionData)
+        this.permissions = result.map((permission) => {
+          const options = JSON.parse(permission.actionData) || []
           permission.checkedAll = false
           permission.selected = []
           permission.indeterminate = false
-          permission.actionsOptions = options.map(option => {
+          permission.actionsOptions = options.map((option) => {
             return {
               label: option.describe,
               value: option.action
@@ -165,5 +177,4 @@ export default {
 </script>
 
 <style scoped>
-
 </style>
